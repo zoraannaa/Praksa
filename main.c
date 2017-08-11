4037aa3518421e678101303eb5a3f9b24c723b1a
zoraannaa <zorman95@gmail.com>
2017-08-11 12:05:51 +0200

    poruka

--- main.c
+++ main.c
@@ -8,6 +8,8 @@ printf("Hello from the other sideeeeeee\n");
 printf("promjena br";)
 printf("Guess who is back");
 printf("This is getting ridiculous");
-printf("aaaaa")
+printf("aaaaa");
+printf("petak je hocu kuci");
+
 return 0;
 }
